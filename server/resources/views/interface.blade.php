eede9c7f1763c81f8b2fe331b65468a4fcd051a2
Hamza Mogni <hamzamogni5@gmail.com>
2020-04-13 19:42:45 +0100

    now serving bot interface using laravel route
    the route is now server_ip/
    
    cobot_interface directory was deleted
    index.html is now at server/resources/interface.blade.php
    assets were moved to public/{css, js, images}
    assets URLs were changed accordingly on index.blade.php

--- server/resources/views/interface.blade.php
+++ server/resources/views/interface.blade.php
@@ -3,17 +3,17 @@
 
 <head>
   <meta charset="utf-8">
-  <link rel="stylesheet" href="chatbot.css">
+  <link rel="stylesheet" href="css/chatbot.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.4.2/react.js" charset="utf-8"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.4.2/react-dom.js" charset="utf-8"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.21.1/babel.min.js" charset="utf-8"></script>
   <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <script src="chatbot.js"></script>
+  <script src="js/chatbot.js"></script>
   <script type="text/babel">
 
     ReactDOM.render(React.createElement(Cobot, {
-      logo: "cobot.png",
+      logo: "images/cobot.png",
       api: "http://35.211.212.220:5005/webhooks/rest/webhook",
       color: "#9883E5",
       initialMessage: "I am a chatbot, ask me!",
