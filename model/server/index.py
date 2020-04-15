7605f750b59483ba0a5a50fe017dc884949cc23d
Hamza Mogni <hamzamogni5@gmail.com>
2020-04-15 21:25:14 +0100

    cleaned imports on index.py

--- model/server/index.py
+++ model/server/index.py
@@ -1,21 +1,12 @@
-from flask import Flask, request, jsonify
-import base64
-from io import BytesIO
-import re
-from PIL import Image, ImageFilter
-from flask import render_template
-
+from flask import Flask, request
 from model import *
 
 app = Flask(__name__)
 
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["POST"])
 def index(name=None):
     if request.method == 'POST':
         message = request.json['message']
-
-        
 
         loaded_model = Net(message)
         result = loaded_model.main()
