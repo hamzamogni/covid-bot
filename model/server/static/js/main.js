2e6ad5ae703a8ef54c9bd4a5ed0cd54cb3fdb68b
Hamza Mogni <hamzamogni5@gmail.com>
2020-04-15 21:20:11 +0100

    deleted unused files in Flask app

--- model/server/static/js/main.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    function drawShape() {
-        var canvas = document.getElementById('canvas');
-        var ctx = canvas.getContext('2d');
-        ctx.fillRect(20, 20, 100, 100);
-    }
-});
