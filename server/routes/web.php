5ea199ad5cad9992a6a2c96a4ab9a63e60c349b1
Hamza Mogni <hamzamogni5@gmail.com>
2020-04-17 21:48:31 +0100

    added laravel /questions route

--- server/routes/web.php
+++ server/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('interface');
 });
+
+Route::get('/questions', function () {
+    return view('questions');
+});
