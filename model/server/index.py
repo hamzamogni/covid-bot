aa8facc51e58f5910de141195ca675958a163202
Hamza Mogni <hamzamogni5@gmail.com>
2020-04-18 13:57:05 +0100

    added crawler route to flask

--- model/server/index.py
+++ model/server/index.py
@@ -1,6 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from model import *
+from Crawler import Crawler
+
 from textblob import TextBlob
+
+
 
 app = Flask(__name__)
 
@@ -20,3 +24,14 @@ def index(name=None):
         loaded_model = Net(message)
         result = loaded_model.main()
         return result
+
+
+@app.route("/crawler", methods=["GET"])
+def crawl(name=None):
+    if request.method == 'GET':
+        country = request.args.get('country')
+        if country == None:
+            return jsonify({"error": "invalid query parameters"})
+        
+        crawler = Crawler()
+        return jsonify(crawler.run(country))
